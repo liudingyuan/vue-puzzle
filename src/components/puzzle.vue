d5bdafdf8abfb22ea6f4a6431e8021e20f04a70d
ldy <ldy1994@outlook.com>
2016-08-20 22:19:47 +0800

    random sort,move puzzle and success judgement

--- src/components/puzzle.vue
+++ src/components/puzzle.vue
@@ -3,19 +3,112 @@
 		<ul class="puzzle-container">
 			<li 
 			v-for="puzzle in puzzles"
-			v-text="puzzle.id"></li>
+			v-text="puzzle.id"
+			@click="move($index)"></li>
 		</ul>
+		<button @click="render">重置</button>
 	</div>
 </template>
 
 <script>
 	export default {
-		data() {
+		data () {
 			return {
-				puzzles: [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id: 7},
-				          {id: 8}, {id: 9}, {id: 10}, {id: 11}, {id: 12}, {id: 13}, 
-				          {id: 14}, {id: 15}]
+				puzzles: []
+			}
+		},
+		ready () {
+			this.render();
+		},
+		methods: {
+			render () {
+				let arr = [];
+				for (let i = 0;i < 15;i++) {
+					arr.push({id: i});
+				}
+				this.puzzles = arr.sort(() => Math.random() - 0.5);
+				this.puzzles.push('');
+			},
+			changePos (curPos, afPos, obj) {
+				this.puzzles.$set(afPos, obj);
+				this.puzzles.$set(curPos, '');
+			},
+			move (index) {
+				let cur = this.puzzles[index],
+				    top = this.puzzles[index - 4],
+				    right = this.puzzles[index + 1],
+				    bottom = this.puzzles[index + 4],
+				    left = this.puzzles[index - 1];
+
+				if (top === '') {
+					this.changePos(index, index - 4, cur);
+				}
+				else if (right === '') {
+					this.changePos(index, index + 1, cur);
+				}
+				else if (bottom === '') {
+					this.changePos(index, index + 4, cur);
+				}
+				else if (left === '') {
+					this.changePos(index, index - 1, cur);
+				}
+			},
+			isSuccess () {
+				if (this.puzzles[10] === '') {
+					let arr = this.puzzles.slice(0, 15);
+
+					if (arr.every((item, index) => item.id === index)) {
+						alert('success!');
+					}
+				}
 			}
 		}
 	}
 </script>
+
+<style lang="sass">
+	.puzzle {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		width: 40%;
+		border: 1px solid #333;
+		-webkit-transform: translate(-50%, -50%);
+		transform: translate(-50%, -50%);
+
+		&::after {
+			content: '';
+			display: block;
+			margin-top: 100%;
+		}
+
+		button {
+			position: absolute;
+			top: 100%;
+			margin-top: 1em;
+			background: #3498db;
+			border: 0;
+			border-radius: 5px;
+			padding: .5em 1em;
+			color: #fff;
+			cursor: pointer;
+		}
+	}
+	@media only screen and (max-width: 568px) {
+		.puzzle {width: 100%;}
+	}
+	.puzzle-container {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+        
+        li {
+        	float: left;
+        	border: 1px solid pink;
+        	list-style: none;
+        	width: 25%;
+        	height: 25%;
+        	cursor: pointer;
+        }
+	}
+</style>
